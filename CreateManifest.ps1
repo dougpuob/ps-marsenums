043210f0fff246027c9ef843beffa75294ecd88d
dougpuob <dougpuob@gmail.com>
2021-02-06 00:46:58 +0800

    Align code content in CreateManifest.ps1

--- CreateManifest.ps1
+++ CreateManifest.ps1
@@ -3,9 +3,9 @@ $Manifest = @{
     RootModule              = 'MarsEnums.psm1'
     Author                  = 'dougpuob'
     CompatiblePSEditions    = 'Desktop'
-	ModuleVersion			= '0.0.1'
+    ModuleVersion           = '0.0.1'
     Description             = 'Enumerations of Mars project.'
     PowerShellVersion       = '7.1'
-	GUID 					= '96ce4539-db25-456d-9b3a-50495b672a48'
+    GUID                    = '96ce4539-db25-456d-9b3a-50495b672a48'
 }
 New-ModuleManifest @Manifest -ErrorAction Stop
